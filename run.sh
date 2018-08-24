5620ff908d17bffae6c1c9e2fc68568b19c8c2c1
Sriram Baskaran <sriram.baskaran92@gmail.com>
2018-08-24 16:52:54 -0700

    Initial commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
+#
+#python ./src/prediction-validation.py ./input/window.txt ./input/actual.txt ./input/predicted.txt ./output/comparison.txt
